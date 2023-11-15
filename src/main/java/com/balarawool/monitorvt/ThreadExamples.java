fa3207c989b2500307691a70f70e15a1ac0996f6
Balkrishna Rawool <balkrishna.rawool@gmail.com>
2023-11-15 23:30:03 +0100

    Added ThreadExamples

---
+++ src/main/java/com/balarawool/monitorvt/ThreadExamples.java
@@ -0,0 +1,21 @@
+package com.balarawool.monitorvt;
+
+import java.util.concurrent.Executors;
+
+public class ThreadExamples {
+    public static void platformThread() {
+    	var t = Thread.ofPlatform().start(() -> System.out.println(Thread.currentThread()));
+    }
+
+    public static void virtualThread() {
+    	try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+    		executor.submit(() -> System.out.println(Thread.currentThread()));
+    		executor.submit(() -> System.out.println(Thread.currentThread()));
+    		executor.submit(() -> System.out.println(Thread.currentThread()));
+    		executor.submit(() -> System.out.println(Thread.currentThread()));
+    		executor.submit(() -> System.out.println(Thread.currentThread()));
+    		executor.submit(() -> System.out.println(Thread.currentThread()));
+    		executor.submit(() -> System.out.println(Thread.currentThread()));
+    	}
+    }
+}
---
+++ src/test/java/com/balarawool/monitorvt/TestThreadExamples.java
@@ -0,0 +1,16 @@
+package com.balarawool.monitorvt;
+
+import org.junit.jupiter.api.Test;
+
+public class TestThreadExamples {
+    @Test
+    public void platformThread() {
+        ThreadExamples.platformThread();
+    }
+
+    @Test
+    public void virtualThread() {
+        ThreadExamples.virtualThread();
+    }
+
+}
